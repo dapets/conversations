a96a0fa058b91ad30eea9befdfd63f7fcb2beb54
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:46 +0100

    Make parallell route start with lowercase character

--- frontend/app/chats/layout.tsx
+++ frontend/app/chats/layout.tsx
@@ -1,16 +1,22 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Chat Window",
+  description: "Chat with other users",
+};
 
 export default async function ClientLayout({
   children,
-  ChatList,
+  chatList,
 }: {
   children: React.ReactNode;
-  ChatList: React.ReactNode;
+  chatList: React.ReactNode;
 }) {
   return (
     <div className="flex h-screen w-screen">
       <nav className="p-4 overflow-y-auto md:w-64 md:block hidden">
-        {ChatList}
+        {chatList}
       </nav>
       {children}
     </div>
