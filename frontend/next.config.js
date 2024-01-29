d5a186533dc8f713511a0d924f653532ddfa0e68
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:49 +0100

    Re-add redirect form / to /chats

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,19 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // logging: {
-  //   fetches: {
-  //     fullUrl: true,
-  //   },
-  // },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/chats",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/chats",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
