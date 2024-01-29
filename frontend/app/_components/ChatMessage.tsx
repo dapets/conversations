adce0f73090dc73ac983fc2dcd9a19396e5c1b5c
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:32 +0100

    Highlight author's chat messages

--- frontend/app/_components/ChatMessage.tsx
+++ frontend/app/_components/ChatMessage.tsx
@@ -1,13 +1,22 @@
-"use client";
-
+import { cn } from "@/lib/utils";
 import dayjs from "dayjs";
 import AdvancedFormat from "dayjs/plugin/relativeTime";
-import { History } from "types/dbEntities";
+import { History, User } from "types/dbEntities";
 
 dayjs.extend(AdvancedFormat);
 
-export function ChatMessage(props: { history: History }) {
+async function getSelf() {
+  const result = await fetch(process.env.BACKEND_URL + "/whoami");
+
+  return JSON.parse(await result.text()) as User;
+}
+
+export async function ChatMessage(props: { history: History }) {
   const { author, sentOn, message } = props.history;
+
+  const iAm = await getSelf();
+  const isAuthor = iAm.id === author.id;
+
   return (
     <section>
       <div className="flex items-baseline">
@@ -18,7 +27,16 @@ export function ChatMessage(props: { history: History }) {
           {dayjs().to(sentOn)}
         </time>
       </div>
-      <div className="flex leading-7 max-w-[75%] rounded-lg px-3 py-2 text-sm bg-primary text-primary-foreground">
+      <div
+        data-isauthor={isAuthor}
+        className={cn(
+          "flex leading-7 max-w-[75%] rounded-lg px-3 py-2 ",
+          "text-sm bg-accent text-accent-foreground",
+          {
+            "bg-primary text-primary-foreground": isAuthor,
+          }
+        )}
+      >
         {message}
       </div>
     </section>
