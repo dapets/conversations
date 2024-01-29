29d237b8434bad9a6f0797521d5d940185c38cc0
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:32 +0100

    Add various error boundaries

---
+++ frontend/app/chats/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ChatsListError() {
+  return "Error fetching chat list";
+}
---
+++ frontend/app/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function GlobalError() {
+  return "A global error occurred. Please try again.";
+}
