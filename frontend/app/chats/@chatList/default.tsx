88cc6109b0539bdba4f966f4acf1e9a222380963
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:48 +0100

    Explain why we're using a default.tsx and not a page.tsx

--- frontend/app/chats/@chatList/default.tsx
+++ frontend/app/chats/@chatList/default.tsx
@@ -1,3 +1,5 @@
+//Needs to be a default.tsx und not a page.tsx because @chatList would be null on routes like /chats/chatter-id
+//more info: https://nextjs.org/docs/app/building-your-application/routing/parallel-routes#defaultjs
 import ChatDescriptionList from "@components/ChatDescriptionList";
 import { getChatList } from "app/actions";
 
