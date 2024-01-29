2b888f2fcc1b2289304be43929a1a68ba840d5af
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:33 +0100

    ChatDescriptionList now accepts chats as a prop

--- frontend/app/_components/ChatDescriptionList.tsx
+++ frontend/app/_components/ChatDescriptionList.tsx
@@ -1,20 +1,14 @@
 import { ChatDescription } from "@components/ChatDescription";
 import { User } from "types/dbEntities";
 
-async function getChatList() {
-  const result = await fetch(process.env.BACKEND_URL + "/chats", {
-    cache: "no-store",
-  });
-  const text = await result.text();
-  return JSON.parse(text) as User[][];
-}
-
-export default async function ChatDescriptionList() {
-  const chats = await getChatList();
-
+export default async function ChatDescriptionList({
+  chatList,
+}: {
+  chatList: User[][];
+}) {
   return (
     <ul className="space-y-1">
-      {chats.map((u, i) => (
+      {chatList.map((u, i) => (
         <li key={i}>
           <ChatDescription user={u[1]} isActive={false} />
         </li>
--- frontend/app/chats/layout.tsx
+++ frontend/app/chats/layout.tsx
@@ -1,15 +1,26 @@
 import ChatDescriptionList from "@components/ChatDescriptionList";
 import React from "react";
+import { User } from "types/dbEntities";
 
-export default function ClientLayout({
+async function getChatList() {
+  const result = await fetch(process.env.BACKEND_URL + "/chats", {
+    cache: "no-store",
+  });
+  const text = await result.text();
+  return JSON.parse(text) as User[][];
+}
+
+export default async function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const chats = await getChatList();
+
   return (
     <div className="flex h-screen w-screen">
       <nav className="p-4 overflow-y-auto md:w-64 md:block hidden">
-        <ChatDescriptionList />
+        <ChatDescriptionList chatList={chats} />
       </nav>
       {children}
     </div>
