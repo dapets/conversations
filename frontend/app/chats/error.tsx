217989d5cd9c17703dac83f90394fe265f31a538
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:52 +0100

    Add error.tsx

---
+++ frontend/app/chats/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ChatsListError() {
+  return (
+    <div className="text-red-400">
+      <p>Error fetching chat list.</p>
+      <p>Please try again.</p>
+    </div>
+  );
+}
