b80dcdf46362d10fcda95e1a730f515d489e68d9
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 21:00:01 +0100

    Fix not centered

--- frontend/app/chats/page.tsx
+++ frontend/app/chats/page.tsx
@@ -1,3 +1,7 @@
 export default function NoChatSelected() {
-  return <section className="flex m-auto">No chat selected</section>;
+  return (
+    <section className="flex m-auto h-full">
+      <p className="m-auto">No chat selected</p>
+    </section>
+  );
 }
