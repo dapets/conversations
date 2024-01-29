e25c4ec53d5f5dc27a2da55cd318aba6a15cc2a6
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 21:00:50 +0100

    Make NoChatSelected a client component

--- frontend/app/chats/page.tsx
+++ frontend/app/chats/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+/** If this is not a client component it sometimes
+ * shows up when navigating between chats. Very weird.*/
 export default function NoChatSelected() {
   return <p className="row-start-2 place-self-center">No chat selected</p>;
 }
