fab644e537b8cdf82830982031c86d77896bcee1
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:47 +0100

    Rename page.tsx to default.tsx
    Because the [...id] catch-all doesn't have a matching

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "/chats",
-                permanent: true
-            }
-        ]
-    }
-}
+  // logging: {
+  //   fetches: {
+  //     fullUrl: true,
+  //   },
+  // },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/chats",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
