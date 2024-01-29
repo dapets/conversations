9314236657811d322a23d49a1d67733b20ea9d4a
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 21:00:25 +0100

    Fix inverted null check

--- backend/Auth/HasFinishedRegistrationHandler.cs
+++ backend/Auth/HasFinishedRegistrationHandler.cs
@@ -3,13 +3,14 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace backend.Auth;
 
-public class HasFinishedRegistrationHandler(IdentityUtils identityUtils) : AuthorizationHandler<HasFinishedRegistrationRequirement>
+public class HasFinishedRegistrationHandler(ILogger<HasFinishedRegistrationHandler> logger, IdentityUtils identityUtils) : AuthorizationHandler<HasFinishedRegistrationRequirement>
 {
   private readonly IdentityUtils identityUtils = identityUtils;
+  private readonly ILogger<HasFinishedRegistrationHandler> logger = logger;
 
   protected async override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasFinishedRegistrationRequirement requirement)
   {
-    if (context.User is not null)
+    if (context.User is null)
     {
       return;
     }
@@ -17,7 +18,7 @@ public class HasFinishedRegistrationHandler(IdentityUtils identityUtils) : Autho
     var user = await identityUtils.GetUserAsync(context.User);
 
     var hasUserFinishedRegistration = !string.IsNullOrEmpty(user.FirstName) && !string.IsNullOrEmpty(user.LastName);
-    Console.WriteLine($"{nameof(hasUserFinishedRegistration)} {hasUserFinishedRegistration}");
+    logger.LogInformation("{hasUserFinishedRegistration} {hasUserFinishedRegistration}", nameof(hasUserFinishedRegistration), hasUserFinishedRegistration);
     if (hasUserFinishedRegistration)
     {
       context.Succeed(requirement);
