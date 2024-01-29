be146fa6f1e5dcc87d81edb371a6280b37843aff
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:50 +0100

    Add [PersonalData] attribute

--- backend/Entities/ApplicationUser.cs
+++ backend/Entities/ApplicationUser.cs
@@ -4,11 +4,15 @@ namespace backend.Entities;
 
 public class ApplicationUser : IdentityUser
 {
+    [PersonalData]
     public string FirstName { get; set; } = string.Empty;
 
+    [PersonalData]
     public string LastName { get; set; } = string.Empty;
 
+    [PersonalData]
     public ICollection<History> SentMessages { get; set; } = new List<History>();
 
+    [PersonalData]
     public ICollection<Chats> Chats { get; set; } = new List<Chats>();
 }
