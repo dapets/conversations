fa6bba5b8c8656b218f49e228f0094fe633fa563
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 21:00:02 +0100

    Add docstring

--- frontend/utils/configuredDayjs.ts
+++ frontend/utils/configuredDayjs.ts
@@ -5,6 +5,12 @@ import UTC from "dayjs/plugin/utc";
 dayjs.extend(AdvancedFormat);
 dayjs.extend(UTC);
 
+/**This only works as long as the server has (is in) the same time zone as the client.
+ *
+ * To pre-render these time stamps for any client in any time zone correctly we would need to consider browser time zone information.
+ *
+ * Kind of overkill at this stage so we don't.
+ */
 export const getRelativeLocalTimeStrFromUtcDate = (date: Date) =>
   configuredDayjs().to(configuredDayjs.utc(date).local());
 export const configuredDayjs = dayjs;
