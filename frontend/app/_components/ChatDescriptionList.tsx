9b0d37f2c949c0fe3cf22ffcfa43f9bad26d8567
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:45 +0100

    Change activeUserId to string

--- frontend/app/_components/ChatDescriptionList.tsx
+++ frontend/app/_components/ChatDescriptionList.tsx
@@ -14,14 +14,18 @@ export default function ChatDescriptionList({
   const pathname = usePathname();
 
   const segments = pathname.split("/");
-  const activeUserId = +segments[2];
+  const activeUserId = segments[2];
 
   return (
     <ul className="space-y-1">
       {chatList.map((u, i) => (
         <li key={i}>
           {/* using u[1] because the first member is (currently) the logged in user */}
-          <ChatDescription user={u[1]} isActive={u[1].id === activeUserId} />
+          <ChatDescription
+            user={u[1]}
+            isActive={u[1].id === activeUserId}
+            lastMessage={lastMessage}
+          />
         </li>
       ))}
     </ul>
