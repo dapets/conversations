456f0f9dd67c1bccb2aad07863782f1d3a785743
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:42 +0100

    Add login form

--- frontend/app/_chatComponents/LoginForm.tsx
+++ frontend/app/_chatComponents/LoginForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -13,23 +15,25 @@ import { Input } from "@shadcn/input";
 export default function LoginForm() {
   return (
     <Card className="rounded-xl bg-card text-card-foreground shadow-sm">
-      <CardHeader className="space-y-1">
-        <CardTitle className="leading-8">Log in</CardTitle>
+      <CardHeader className="flex flex-col space-y-1.5 p-6">
+        <CardTitle className="text-2xl font-semibold leading-none tracking-tight">
+          Create an account
+        </CardTitle>
         <CardDescription className="">
-          Enter your email below to log into your account
+          Enter your email below to create your account
         </CardDescription>
       </CardHeader>
       <CardContent className="p-6 pt-0 grid gap-4">
         <div className="grid gap-2">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" />
+          <Input id="email" type="email" placeholder="max@example.com" />
         </div>
         <div className="grid gap-2">
           <Label htmlFor="password">Password</Label>
           <Input id="password" type="password" />
         </div>
       </CardContent>
-      <CardFooter className="">
+      <CardFooter>
         <Button className="w-full">Login</Button>
       </CardFooter>
     </Card>
