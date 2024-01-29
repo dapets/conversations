77b5b3a8cf0ca565e093290e93c8f679de68bc5e
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 21:00:22 +0100

    Make active empty input not red

--- frontend/@/components/ui/input.tsx
+++ frontend/@/components/ui/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         type={type}
         className={cn(
           "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          "focus-visible:invalid:ring-2 focus-visible:invalid:ring-destructive [&:user-invalid]:ring-1 [&:user-invalid]:ring-destructive",
+          "focus-visible:invalid:ring-2 focus-visible:invalid:ring-destructive focus-visible:[&:placeholder-shown]:ring-primary [&:user-invalid]:ring-1 [&:user-invalid]:ring-destructive",
           className,
         )}
         ref={ref}
