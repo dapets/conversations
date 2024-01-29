37c975396a5ac61fda2e530a11057a07b5ef08ab
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 21:00:50 +0100

    Change test instance warning

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
               <div className="grid h-[100dvh] w-[100dvw] grid-rows-[minmax(0,auto),minmax(0,1fr)]">
                 {process.env.IS_TEST_INSTANCE && (
                   <p className="text-md my-2 ml-4 mr-6 rounded-lg bg-accent p-1 px-2 text-center font-mono font-medium leading-5 lg:mr-4">
-                    This is a test instance. The database resets every hour.
+                    This is a test instance. New conversations reset every hour.
                   </p>
                 )}
                 {children}
