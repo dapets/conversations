817098a096cd463c0ba2ba74f2a68c5bce95a430
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:29 +0100

    Add db context and serialization

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,28 @@
+using System.Text.Json.Serialization;
+using backend.Entities;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DevContext>();
+builder.Services.Configure<JsonOptions>(options =>
+{
+    // options.SerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+}
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/chats", (DevContext db) => db
+    .Users
+    .Include(u => u.Chats)
+    .ThenInclude(c => c.Members)
+    .First());
 
 app.Run();
