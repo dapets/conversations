66b28f79c86402c66ffcb18ce40d67f18995d383
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:31 +0100

    Not caching the user's chats for now

--- frontend/app/_components/ChatDescriptionList.tsx
+++ frontend/app/_components/ChatDescriptionList.tsx
@@ -2,7 +2,9 @@ import { ChatDescription } from "@components/ChatDescription";
 import { User } from "types/dbEntities";
 
 async function getChatList() {
-  const result = await fetch(process.env.BACKEND_URL + "/chats");
+  const result = await fetch(process.env.BACKEND_URL + "/chats", {
+    cache: "no-store",
+  });
   const text = await result.text();
   return JSON.parse(text) as User[][];
 }
