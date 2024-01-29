21a09163e5e7abbb90df48dba527baf01294068a
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:59 +0100

    Return nan instead of throwing

--- frontend/utils/utils.ts
+++ frontend/utils/utils.ts
@@ -1,8 +1,6 @@
 export function getActiveChatRoomId(pathname: string) {
   const segments = pathname.split("/");
   const activeChatRoomId = +segments[2];
-  if (!activeChatRoomId) {
-    throw Error("Can't extract chatRoomId from pathname: invalid pathname");
-  }
+
   return activeChatRoomId;
 }
