3c5d35e9f7b814e40c429d5809280bbf9b63585f
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 21:00:01 +0100

    Change nav bar size

--- frontend/app/chats/layout.tsx
+++ frontend/app/chats/layout.tsx
@@ -15,7 +15,7 @@ export default async function ClientLayout({
 }) {
   return (
     <div className="flex h-[100dvh] w-[100dvw]">
-      <nav className="basis-1/4 shrink-0 overflow-y-auto md:block hidden">
+      <nav className="basis-1/3 max-w-sm shrink-0 overflow-y-auto md:block hidden">
         {chatList}
       </nav>
       <section className="flex-grow overflow-y-auto p-4 pl-0 pr-0">
