55256a1dcc404f149c0b64754f9db83da886e31e
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:29 +0100

    Move frontend code to /frontend

---
+++ frontend/app/chats/[[...name]]/page.tsx
@@ -0,0 +1,33 @@
+import { ChatMessage } from "app/_components/ChatMessage";
+import { sampleChatHistory } from "utils/sampleData";
+
+export default function ChatHistory({
+  params,
+}: {
+  params: { name: string[] };
+}) {
+  if (!("name" in params)) {
+    return <section className="flex m-auto">No chat selected</section>;
+  }
+
+  const name = decodeURIComponent(params.name[0]);
+
+  const authorIdx = sampleChatHistory.findIndex((n) => n.author === name);
+  if (authorIdx < 0) return "Invalid Author: error fetching messages";
+
+  return (
+    <section className="overflow-y-auto">
+      <ul className="flex flex-col space-y-4">
+        {sampleChatHistory[authorIdx].messages.map((m, i) => (
+          <li key={i}>
+            <ChatMessage
+              author={sampleChatHistory[authorIdx].author}
+              message={m}
+              sentOn={new Date(Date.parse(sampleChatHistory[authorIdx].sentOn))}
+            />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
