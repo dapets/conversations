4ece3c48e64ec82c80291b8dfa311df98435c78e
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:59 +0100

    Parse History.SendOn correctly

--- frontend/app/_components/Message.tsx
+++ frontend/app/_components/Message.tsx
@@ -1,9 +1,11 @@
 import { cn } from "@/lib/utils";
 import dayjs from "dayjs";
 import AdvancedFormat from "dayjs/plugin/relativeTime";
+import UTC from "dayjs/plugin/utc";
 import { HistoryEntity } from "utils/dbEntities";
 
 dayjs.extend(AdvancedFormat);
+dayjs.extend(UTC);
 
 export function Message({
   history,
@@ -23,7 +25,7 @@ export function Message({
           {author.firstName + " " + author.lastName}
         </h2>
         <time className="text-sm text-zinc-600 ml-4" suppressHydrationWarning>
-          {dayjs().to(sentOn)}
+          {dayjs().to(dayjs.utc(sentOn).local())}
         </time>
       </div>
       <div
