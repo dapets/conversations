e95001b509b0f906a6b667087f1dc69f5ef1461a
David Petscher <72125194+dailydoseofdoge@users.noreply.github.com>
2024-01-29 20:59:33 +0100

    Highlight active User

--- frontend/app/_components/ChatDescriptionList.tsx
+++ frontend/app/_components/ChatDescriptionList.tsx
@@ -1,16 +1,25 @@
+"use client";
+
 import { ChatDescription } from "@components/ChatDescription";
+import { usePathname } from "next/navigation";
 import { User } from "types/dbEntities";
 
-export default async function ChatDescriptionList({
+export default function ChatDescriptionList({
   chatList,
 }: {
   chatList: User[][];
 }) {
+  const pathname = usePathname();
+
+  const segments = pathname.split("/");
+  const activeUserId = +segments[2];
+
   return (
     <ul className="space-y-1">
       {chatList.map((u, i) => (
         <li key={i}>
-          <ChatDescription user={u[1]} isActive={false} />
+          {/* using u[1] because the first member is (currently) the logged in user */}
+          <ChatDescription user={u[1]} isActive={u[1].id === activeUserId} />
         </li>
       ))}
     </ul>
